d985f21b1ab4b3741dc635ddc475375dacbe7b6f
felipe-franca <felipe.paulino57@gmail.com>
2022-04-11 21:28:41 -0300

    incrementing project

--- EscolaIdiomas/src/escolaidiomas/EscolaIdiomas.java
+++ EscolaIdiomas/src/escolaidiomas/EscolaIdiomas.java
@@ -4,42 +4,117 @@ import javax.swing.JOptionPane;
 
 public class EscolaIdiomas {
   public static void main(String[] args) {
-    int op = 0;
+    int op = 0, list = 0, teachersList = 0;
+    // String[][] scholl = new String[][];
+
+    msg("========================================="
+        + "\n\nBem vindo a escola de Idiomas JDialog"
+        + "\n\nA melhor escolha para se aprender outro idioma !"
+        + "\nVocê é novo administrador do sistema"
+        + "\nRealize o cadastro de alunos e professores para nova turma !"
+        + "\n\n=========================================", false);
+
     do {
-      op = Integer.parseInt(JOptionPane.showInputDialog("Digite o número da operação:" +
-          "\n1 - Cadastrar Aluno\n2 - Cadastrar Professor"));
+      try {
+        op = Integer.parseInt(input(buildOptions(list, teachersList)));
+
+        if (op > 2 && (list == 0 || teachersList == 0))
+          throw new NumberFormatException("Out of range.");
+
+      } catch (NumberFormatException e) {
+        print(e.getMessage());
+        msg("Ops! Opção invalida, tente novamente !", true);
+      }
+
       switch (op) {
         case 1:
-          String n;
-          n = JOptionPane.showInputDialog("Digite o Nome do Aluno: ");
-          System.out.println("Nome: " + n);
-          String s;
-          s = JOptionPane.showInputDialog("Digite o Sobrenome: ");
-          System.out.println("Sobrenome: " + s);
-          String r;
-          r = JOptionPane.showInputDialog("Digite o Rg: ");
-          System.out.println("Rg: " + r);
-          String m;
-          m = JOptionPane.showInputDialog("Digite o código da Matrícula: ");
-          System.out.println("Matrícula: " + m);
-          String c;
-          c = JOptionPane.showInputDialog("Digite a Classe: ");
-          System.out.println("Classe: " + c);
+          String firstName;
+          firstName = input("Digite o Nome do Aluno: ");
+          print("Nome: " + firstName);
+
+          String lastName;
+          lastName = input("Digite o Sobrenome: ");
+          print("Sobrenome: " + lastName);
+
+          String socialNumber;
+          socialNumber = input("Digite o Rg: ");
+          print("Rg: " + socialNumber);
+
+          String registration;
+          registration = input("Digite o código da Matrícula: ");
+          print("Matrícula: " + registration);
+
+          String studentClass;
+          studentClass = input("Digite a Classe: ");
+          print("Classe: " + studentClass);
+          list += 1;
           break;
+
         case 2:
-          String p;
-          p = JOptionPane.showInputDialog("Digite o Nome Professor:");
-          System.out.println("Professor: " + p);
-          String e;
-          e = JOptionPane.showInputDialog("Digitar a especialidade: ");
-          System.out.println("Especialista em: " + e);
-          String t;
-          t = JOptionPane.showInputDialog("Digite o Turno da Aula: ");
-          System.out.println("Turno: " + t);
+          String teachersName;
+          teachersName = input("Digite o Nome Professor:");
+          print("Professor: " + teachersName);
+
+          String teaches;
+          teaches = input("Digitar a especialidade: ");
+          print("Especialista em: " + teaches);
+
+          String time;
+          time = input("Digite o Turno da Aula: ");
+          print("Turno: " + time);
+          teachersList += 1;
           break;
+        case 3:
+          if (list == 0)
+            break;
+          else
+            msg("imprimir alunos matriculados", false);
+            continue;
+        case 4:
+          if (teachersList == 0)
+            break;
+          else
+            msg("imprimir professores matriculados", false);
+            continue;
         case 0:
-          JOptionPane.showMessageDialog(null, "Saindo do Sistema");
+          int confirm = confirm("Deseja mesmo Sair ?");
+          if (confirm > 0)
+            op = 10;
+          else
+            msg("Saindo do Sistema", false);
       }
     } while (op != 0);
+  }
+
+  private static void msg(String aMessage, Boolean isWarn) {
+    JOptionPane.showMessageDialog(null, aMessage, "Escola de Idiomas",
+        isWarn ? JOptionPane.WARNING_MESSAGE : JOptionPane.PLAIN_MESSAGE);
+  }
+
+  private static String input(String aMessage) {
+    return JOptionPane.showInputDialog(aMessage);
+  }
+
+  private static int confirm(String aMessage) {
+    return JOptionPane.showConfirmDialog(null, aMessage, "Escola de Idiomoas", JOptionPane.YES_NO_OPTION);
+  }
+
+  public static void print(String aMessage) {
+    System.out.println(aMessage);
+  }
+
+  private static String buildOptions(int students, int teachers) {
+    String msg = "";
+    if (students == 0 && teachers == 0) {
+      msg = "Digite o número da operação:\n1 - Cadastrar Aluno\n2 - Cadastrar Professor\n0 - Sair";
+    } else if (students > 0 && teachers == 0) {
+      msg = "Digite o número da operação:\n1 - Cadastrar Aluno\n2 - Cadastrar Professor\n3 - Lista de alunos\n0 - Sair";
+    } else if (students == 0 && teachers > 0){
+      msg = "Digite o número da operação:\n1 - Cadastrar Aluno\n2 - Cadastrar Professor\n4 - Lista de professorres\n0 - Sair";
+    } else {
+      msg = "Digite o número da operação:\n1 - Cadastrar Aluno\n2 - Cadastrar Professor\n3 - Lista de alunos\n4 - Lista de professorres\n0 - Sair";
+    }
+
+    return msg;
   }
 }
